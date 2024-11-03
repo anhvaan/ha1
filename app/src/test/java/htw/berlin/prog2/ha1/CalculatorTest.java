37e68f851303205468104f62dc3627b165d77fbf
anhvaan <ThiAnh.Vu@Student.HTW-Berlin.de>
2024-11-03 16:56:47 +0100

    1. Commit für neuer grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,22 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("should display result after subtracting two positive multi-digit numbers")
+    void testPositiveSubtraction() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(3);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(1);
+        calc.pressEqualsKey();
+
+        String expected = "19";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
