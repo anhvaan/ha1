bf31d91824beb09f443893f1f353629dd8f80e7b
anhvaan <ThiAnh.Vu@Student.HTW-Berlin.de>
2024-11-03 17:02:19 +0100

    2. Commit für neuer roter Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -107,5 +107,19 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("should display error when dividing by zero with Inversion")
+    void testDivisionByZeroInversion() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("1/x");
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
